ad0d6b9dd1c0b7d15b7008b5b038d709b9d254b8
Iso <kagiso.k.mashigo@gmial.com>
2020-11-03 18:17:47 +0000

    cats files updated and added error handling

---
+++ asyncBreeds.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+const breedDetailsFromFile = function(breed, returnBreedData) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    console.log("In readFile's Callback: it has the data.");
+    if (!error) {
+      returnBreedData(data);
+    } else {
+      console.log(error);
+    }
+  });
+};
+
+const printCatbreed = breed => {console.log('Return Value: ', breed)};
+
+breedDetailsFromFile('Bombay', printCatbreed);
+
